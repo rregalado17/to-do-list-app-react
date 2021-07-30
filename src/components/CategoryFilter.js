5252eb39320f79ecffff553d6376bab944544e44
rregalado17 <rregalado17@gmail.com>
2021-07-30 00:27:59 -0400

    build a to do list app

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CategoryFilter() {
+
+function CategoryFilter( {categories} ) {
+
+  const [selectedCategory, setSelectedCategory] = useState('All')
+
+  const categoryFilter = categories.filter((category) => {
+    if (category === 'All') return true;
+    return category === selectedCategory
+  }) 
+
+  function handleClick(event){
+    setSelectedCategory(event.target.value)
+  }
+
+
+  const categoryList = categories.map((category) => 
+    <button key={category} onClick={handleClick}>{category}</button>
+  )
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {categoryList}
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,19 @@
 import React from "react";
+import Task from "./Task"
 
-function TaskList() {
+function TaskList( {tasksList} ) {
+
+  const tasks = tasksList.map((task) => (
+    <Task 
+    key={task.text}
+    text={task.text} 
+    category={task.category}
+    />
+  ))
   return (
+    
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {tasks} 
     </div>
   );
 }
